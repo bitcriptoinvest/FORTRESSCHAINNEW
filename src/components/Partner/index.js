3b5fcadb842cd71b6ed862cfb8b831fda0d5c593
bitcriptoinvest <145938809+bitcriptoinvest@users.noreply.github.com>
2024-01-13 01:52:44 -0300

    Update index.js

--- src/components/Partner/index.js
+++ src/components/Partner/index.js
@@ -1,15 +1,34 @@
+import styled from 'styled-components'
+import blizzard from "../../assets/images/blizzard.png"
 import c98 from '../../assets/images/c98.png'
-
+import crypto from '../../assets/images/crypto.png'
 import fpt from '../../assets/images/fpt.png'
-
+import gateio from '../../assets/images/gateio.png'
+import gbv from '../../assets/images/gbv.png'
 import guildfi from '../../assets/images/guildfi.png'
 import hashed from '../../assets/images/hashed.png'
-
-
+import impossible from '../../assets/images/impossible.png'
+import istari from '../../assets/images/istari.png'
+import k300 from '../../assets/images/k300.png'
+import kucoin from '../../assets/images/kucoin.png'
+import kyber from '../../assets/images/kyber.png'
+import mexc from '../../assets/images/mexc.png'
+import ngc from '../../assets/images/ngc.png'
+import snz from '../../assets/images/snz.png'
+import tpx from '../../assets/images/tpx.png'
+import vanda from '../../assets/images/vanda.png'
+import deo from '../../assets/images/deo.png'
+import ecomobi from '../../assets/images/ecomobi.png'
+import ahamove from '../../assets/images/ahamove.png'
+import redline from '../../assets/images/redline.png'
+import dwf from '../../assets/images/dwf.png'
+import oig from '../../assets/images/oig.png'
+import pencil from '../../assets/images/pencil.png'
+import matchbox from '../../assets/images/matchbox.png'
 import ld from '../../assets/images/ld.png'
 import { useState } from 'react'
 const Wrap = styled.div`
-.title {
+    > .title {
         font-weight: 600;
         font-size: 40px;
         line-height: 42px;
@@ -26,20 +45,20 @@ const Wrap = styled.div`
         -webkit-text-fill-color: transparent;
         background-clip: text;
     }
-div {
+    > div {
         margin: 0px auto;
 
         width: 1008px;
         display: flex;
         justify-content: space-between;
         align-items: center;
-img {
+        > img {
             opacity: 0.6;
             transition: all 0.3s ease;
             margin-top: 35px;
             max-width: 130px
         }
-img:hover {
+        > img:hover {
             opacity: 1;
             -webkit-filter: drop-shadow(rgba(255, 255, 255, 0.6) 0px 0px 20px);
             filter: drop-shadow(rgba(255, 255, 255, 0.6) 0px 0px 20px);
@@ -62,17 +81,21 @@ img:hover {
         text-align: center;
     }
     @media only screen and (max-width: 1000px) {
-div {
+        > div {
             flex-wrap: wrap;
             width: unset;
             padding: 0px 14px;
             margin-top: 32px;
             justify-content: space-evenly;
-img {
+            > img {
                 transform: scale(0.8);
             }
         }
     }
+`
+export default function Partner() {
+    const [open, setOpen] = useState(false)
+    return (
         <Wrap>
             <h3 className='title'>Honored to be accompanied with</h3>
             <div>
@@ -80,10 +103,45 @@ img {
                 <img src={hashed} alt='' />
                 <img src={c98} alt='' />
                 <img src={guildfi} alt='' />
+                <img src={crypto} alt='' />
                 <img src={ld} alt='' />
             </div>
-
-
+            <div>
+            <img src={matchbox} alt='' />
+                <img src={istari} alt='' />
+                <img src={impossible} alt='' />
+                <img src={gbv} alt='' />
+                <img src={kyber} alt='' />
+                <img src={ngc} alt='' />
+            </div>
+            {open ? (
+                <>
+                    <div>
+                        <img src={kucoin} alt='' />
+                        <img src={snz} alt='' />
+                        <img src={k300} alt='' />
+                        <img src={blizzard} alt='' />
+                        <img src={mexc} alt='' />
+                    </div>
+                    <div>
+                        <img src={gateio} alt='' />
+                        <img src={tpx} alt='' />
+                        <img src={vanda} alt='' />
+                        <img src={deo} alt='' />
+                        <img src={ecomobi} alt='' />
+                    </div>
+                    <div>
+                        <img src={ahamove} alt='' />
+                        <img src={redline} alt='' />
+                        <img src={dwf} alt='' />
+                        <img src={oig} alt='' />
+                        <img src={pencil} alt='' />
+                    </div>
+                    <p>and many others</p>
+                </>
+            ) : (
+                <button onClick={() => setOpen(!open)}>See all </button>
+            )}
         </Wrap>
     )
 }
