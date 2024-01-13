485cf0d335e696da34f935235f4949b83a4788ff
bitcriptoinvest <145938809+bitcriptoinvest@users.noreply.github.com>
2024-01-13 01:45:59 -0300

    Update index.js

--- src/components/Partner/index.js
+++ src/components/Partner/index.js
@@ -2,12 +2,33 @@ import styled from 'styled-components'
 import blizzard from "../../assets/images/blizzard.png"
 import c98 from '../../assets/images/c98.png'
 import crypto from '../../assets/images/crypto.png'
+import fpt from '../../assets/images/fpt.png'
+import gateio from '../../assets/images/gateio.png'
+import gbv from '../../assets/images/gbv.png'
 import guildfi from '../../assets/images/guildfi.png'
 import hashed from '../../assets/images/hashed.png'
+import impossible from '../../assets/images/impossible.png'
+import istari from '../../assets/images/istari.png'
+import k300 from '../../assets/images/k300.png'
+import kucoin from '../../assets/images/kucoin.png'
+import kyber from '../../assets/images/kyber.png'
+import mexc from '../../assets/images/mexc.png'
+import ngc from '../../assets/images/ngc.png'
+import snz from '../../assets/images/snz.png'
+import tpx from '../../assets/images/tpx.png'
+import vanda from '../../assets/images/vanda.png'
+import deo from '../../assets/images/deo.png'
+import ecomobi from '../../assets/images/ecomobi.png'
+import ahamove from '../../assets/images/ahamove.png'
+import redline from '../../assets/images/redline.png'
+import dwf from '../../assets/images/dwf.png'
+import oig from '../../assets/images/oig.png'
+import pencil from '../../assets/images/pencil.png'
+import matchbox from '../../assets/images/matchbox.png'
 import ld from '../../assets/images/ld.png'
 import { useState } from 'react'
 const Wrap = styled.div`
-    > .title {
+.title {
         font-weight: 600;
         font-size: 40px;
         line-height: 42px;
@@ -24,20 +45,20 @@ const Wrap = styled.div`
         -webkit-text-fill-color: transparent;
         background-clip: text;
     }
-    > div {
+div {
         margin: 0px auto;
 
         width: 1008px;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        > img {
+img {
             opacity: 0.6;
             transition: all 0.3s ease;
             margin-top: 35px;
             max-width: 130px
         }
-        > img:hover {
+img:hover {
             opacity: 1;
             -webkit-filter: drop-shadow(rgba(255, 255, 255, 0.6) 0px 0px 20px);
             filter: drop-shadow(rgba(255, 255, 255, 0.6) 0px 0px 20px);
@@ -60,27 +81,28 @@ const Wrap = styled.div`
         text-align: center;
     }
     @media only screen and (max-width: 1000px) {
-        > div {
+div {
             flex-wrap: wrap;
             width: unset;
             padding: 0px 14px;
             margin-top: 32px;
             justify-content: space-evenly;
-            > img {
+img {
                 transform: scale(0.8);
             }
         }
     }
-`
-export default function Partner() {
+        <Wrap>
             <h3 className='title'>Honored to be accompanied with</h3>
             <div>
                 <img src={fpt} alt='' />
                 <img src={hashed} alt='' />
                 <img src={c98} alt='' />
                 <img src={guildfi} alt='' />
-                <img src={crypto} alt='' />
                 <img src={ld} alt='' />
             </div>
+
+
+        </Wrap>
     )
 }
